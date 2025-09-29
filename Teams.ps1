5569e837b3608ad9be67f6113674e63c0a9093f7
t33388528-hue <t33388528@gmail.com>
2025-09-29 10:01:08 +0200

    Update Teams.ps1
    usable now

--- Teams.ps1
+++ Teams.ps1
@@ -1,1 +1,1 @@
-winget install --id Microsoft.Teams -e --accept-source-agreements --accept-package-agreements ; Start-Process "Teams"
+winget install --id Microsoft.Teams -e --accept-source-agreements --accept-package-agreements 
