c719640155da182b0ad40a6928317bd0305d3d49
t33388528-hue <t33388528@gmail.com>
2025-11-19 14:47:49 +0100

    Update Win11Setup.ps1

--- Win11Setup.ps1
+++ Win11Setup.ps1
@@ -12,15 +12,25 @@ $UpdateSearcher = $UpdateSession.CreateUpdateSearcher()
 # Search for updates
 $SearchResult = $UpdateSearcher.Search("IsInstalled=0")
 
+# Filter only KB updates
+$KBUpdates = @()
 foreach ($update in $SearchResult.Updates) {
-    Write-Host "- $($update.Title)"
+    if ($update.Title -match "KB\d+") {
+        $KBUpdates += $update
+        Write-Host "- $($update.Title)"
+    }
 }
 
-Write-Host "Found and downloading $($SearchResult.Updates.Count) updates..."
+if ($KBUpdates.Count -eq 0) {
+    Write-Host "No updates found."
+    return
+}
 
-# Install all available updates
+Write-Host "Starting download and installation of $($KBUpdates.Count) updates..."
+
+# Collect KB updates
 $UpdatesToDownload = New-Object -ComObject Microsoft.Update.UpdateColl
-foreach ($update in $SearchResult.Updates) {
+foreach ($update in $KBUpdates) {
     $UpdatesToDownload.Add($update) | Out-Null
 }
 
