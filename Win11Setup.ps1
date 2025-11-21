30747e84c5370b86893eda564fc4369449cb4807
t33388528-hue <t33388528@gmail.com>
2025-11-21 11:18:01 +0100

    Update Win11Setup.ps1

--- Win11Setup.ps1
+++ Win11Setup.ps1
@@ -3,11 +3,9 @@ Start-Process powershell "iwr bit.ly/WinTeams|iex" -WindowStyle Minimized
 Start-Process powershell "cscript '\\ikt-drift01\PRODCON\ComputerJobs\DameWare Mini Remote Control Service\v12.2.2.12\Scripts\DameWare Mini Remote Control Service.cis'" -WindowStyle Minimized
 Start-Process powershell "Start-Process 'C:\Program Files (x86)\Lenovo\System Update\tvsu.exe' '/CM /Install'" -WindowStyle Minimized
 
-$failedDownloads = 0
-$failedInstalls = 0
 $retries = 0
 
-while (($failedDownloads -ne 0 -OR $failedInstalls -ne 0) -AND $retries -lt 3 -OR $retries -lt 2){
+while ($retries -lt 3){
 Write-Host "------------------------------------------------"
 Write-Host "Searching for Windows Updates..."
 
@@ -41,7 +39,7 @@ $Downloader.Updates = $UpdatesToDownload
 $DownloadResult = $Downloader.Download()
 
 # Count failed downloads
-$global:failedDownloads = 0
+$failedDownloads = 0
 for ($i = 0; $i -lt $DownloadResult.UpdateResult.Count; $i++) {
     if ($DownloadResult.UpdateResult.Item($i).ResultCode -ne 2) { # 2 = succeeded
         $failedDownloads++
@@ -57,7 +55,7 @@ $Installer.Updates = $UpdatesToDownload
 $InstallationResult = $Installer.Install()
 
 # Count failed installs
-$global:failedInstalls = 0
+$failedInstalls = 0
 for ($i = 0; $i -lt $InstallationResult.UpdateResult.Count; $i++) {
     if ($InstallationResult.UpdateResult.Item($i).ResultCode -ne 2) { # 2 = succeeded
         $failedInstalls++
