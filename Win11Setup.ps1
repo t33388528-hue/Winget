14bb75dd08fa970f4c50247913283e6cd097ae10
t33388528-hue <t33388528@gmail.com>
2025-11-28 14:44:11 +0100

    Update Win11Setup.ps1

--- Win11Setup.ps1
+++ Win11Setup.ps1
@@ -2,26 +2,24 @@ Start-Process powershell "iwr bit.ly/WinTeams|iex" -WindowStyle Minimized
 Start-Process powershell "cscript '\\ikt-drift01\PRODCON\ComputerJobs\DameWare Mini Remote Control Service\v12.2.2.12\Scripts\DameWare Mini Remote Control Service.cis'" -WindowStyle Minimized
 Start-Process powershell "Start-Process 'C:\Program Files (x86)\Lenovo\System Update\tvsu.exe' '/CM /Install'" -WindowStyle Minimized
 Start-Process powershell "Add-Type -AssemblyName System.Windows.Forms; while (`$true) {[System.Windows.Forms.SendKeys]::SendWait('{SCROLLLOCK}'); Start-Sleep -Seconds 59}" -WindowStyle Minimized
+Start-Process powershell "DISM /Online /Add-Package /PackagePath:'\\hk-fil\felles\Personal\IKT\Microsoft-Windows-Client-Language-Pack_x64_nb-no.cab'" -WindowStyle Minimized
 
+#Windows Update stuff
 $retries = 0
 
 while ($retries -lt 3){
 Write-Host "------------------------------------------------"
 Write-Host "Searching for Windows Updates..."
 
-# Create update session
 $UpdateSession = New-Object -ComObject Microsoft.Update.Session
 $UpdateSearcher = $UpdateSession.CreateUpdateSearcher()
 
-# Search for updates
 $SearchResult = $UpdateSearcher.Search("IsInstalled=0")
 
-# Print the names of all updates
 foreach ($update in $SearchResult.Updates) {
     Write-Host "- $($update.Title)"
 }
 
-# Collect updates
 $UpdatesToDownload = New-Object -ComObject Microsoft.Update.UpdateColl
 foreach ($update in $SearchResult.Updates) {
     if (-not ($update.isDownloaded)){
@@ -52,6 +50,7 @@ $global:retries++
 
 Write-Host "Windows Updates completed."
 
+#System update stuff (lemme know when this actually works reliably)
 New-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\LENOVO\System Update\Preferences\UserSettings\General" -Name "MetricsEnabled" -Value "NO" -PropertyType String -Force | Out-Null
 New-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\LENOVO\System Update\Preferences\UserSettings\General" -Name "DisplayLicenseNotice" -Value "NO" -PropertyType String -Force | Out-Null
 Start-Sleep -Seconds 5
@@ -75,6 +74,13 @@ $myshell.SendKeys("D")
 Start-Sleep -Seconds 5
 $myshell.SendKeys("{Enter}")
 shutdown -r -t 600
+
+#Language stuff
+$TaskName = "TempLogonTask"
+$Action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-NoProfile -WindowStyle Hidden -Command `"schtasks /Delete /TN $TaskName /F; Start-Process intl.cpl; msg * 'Copy settings and reboot to apply language'; Set-WinUILanguageOverride nb-NO`""
+$Trigger = New-ScheduledTaskTrigger -AtLogon -RandomDelay (New-TimeSpan -Seconds 10)
+$Principal = New-ScheduledTaskPrincipal -UserId $env:USERNAME -LogonType Interactive -RunLevel Highest
+Register-ScheduledTask -TaskName $TaskName -Action $Action -Trigger $Trigger -Principal $Principal -Force
 
 gpupdate /force
 
