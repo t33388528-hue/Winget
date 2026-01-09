7568b6b4ea1e632e2eeca90f9d230b8c164abdf1
t33388528-hue <t33388528@gmail.com>
2026-01-09 12:21:38 +0100

    Create ActiveScreen.ps1

---
+++ ActiveScreen.ps1
@@ -0,0 +1,1 @@
+Start-Process powershell "Add-Type -AssemblyName System.Windows.Forms; while (`$true) {[System.Windows.Forms.SendKeys]::SendWait('{SCROLLLOCK}'); Start-Sleep -Seconds 59}" -WindowStyle Minimized
