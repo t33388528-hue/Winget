07c618d92bb1777abc12a735134c9a76db877fea
t33388528-hue <t33388528@gmail.com>
2026-01-12 12:15:43 +0100

    Update ActiveScreen.ps1

--- ActiveScreen.ps1
+++ ActiveScreen.ps1
@@ -1,1 +1,1 @@
-Start-Process powershell "Add-Type -AssemblyName System.Windows.Forms; while (`$true) {[System.Windows.Forms.SendKeys]::SendWait('{SCROLLLOCK}'); Start-Sleep -Seconds 59}" -WindowStyle Minimized
+Start-Process powershell 'Add-Type -AssemblyName System.Windows.Forms; while (`$true) {[System.Windows.Forms.SendKeys]::SendWait(''{SCROLLLOCK}''); Start-Sleep -Seconds 59}' -WindowStyle Minimized
